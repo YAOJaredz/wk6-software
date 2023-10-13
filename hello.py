0ddc3ec921a2a95b0f4c5972c1e05196a748ebce
YAOJaredz <yyao01@wesleyan.edu>
2023-10-13 15:33:35 -0400

    Created hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+def hello(name : str) -> None:
+    """ This function will take a name and print Hello to the name.
+    
+    Args:
+        name (str): The name appeared in the output.
+    """
+
+    print(f'Hello, {name}!')
+
+if __name__ == '__main__':
+    hello('Jared')
